b3575af5461fde10d53277e1c38d0c38c1a775fb
Damian Malysza <damian.malysza@gmail.com>
2021-11-03 21:17:54 -0400

    commit tree 49771c2ec3834d3df3077447ba426bdeb65d75e7

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+function saturdayFun(activity='roller-skate'){
+  return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office"){
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = "*"){
+  return function(param = "special"){
+    return `You are ${flair}${param}${flair}!`
+  }
+}
+
+const Calculator = {
+  "add" : () => 1 + 3,
+  "subtract" : () => 1 - 3,
+  "multiply" : () => 1 * 3,
+  "divide" : () => 10 / 5
+}
+
+function actionApplyer(int,array){
+  if (array.length === 0) {return int}
+  let final = int
+  for (let f of array) {
+    final = f(final)
+  }
+  return final
+}
